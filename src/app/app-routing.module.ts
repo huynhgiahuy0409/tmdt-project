d8a943d47f2f130c2cec4cc1c2ce411e6e6952f2
doitino <doitino@users.noreply.github.com>
2021-10-16 13:28:54 +0700

    html css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
+import {ProductsComponent} from "./products/products.component";
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {CommonModule} from "@angular/common";
 import {HomePageComponent} from "./home-page/home-page.component";
 import {ListProductComponent} from "./products/list-product/list-product.component";
-import {ProductsComponent} from "./products/products.component";
 
 const routes: Routes = [
-<<<<<<< Updated upstream
-=======
   // { path: '',   redirectTo: '/homepage', pathMatch: 'full' },
   {path: 'homepage', component: HomePageComponent},
   {
@@ -17,11 +16,14 @@ const routes: Routes = [
       }
     ],
   },
->>>>>>> Stashed changes
 ];
 
+
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  imports: [
+    CommonModule,RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule]
+
 })
 export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
-import { MatIconModule } from '@angular/material/icon';
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< Updated upstream
 import { MaterialModules } from './material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-=======
 import { ProductsComponent } from './products/products.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { ListProductComponent } from './products/list-product/list-product.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
->>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
