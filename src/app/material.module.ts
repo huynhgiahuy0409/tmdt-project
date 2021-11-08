eede06433132da2ed61dfb836d12c92bb266bee2
huynhgiahuy0409 <18130094@st.hcmuaf.edu.vn>
2021-11-08 11:47:25 +0700

    account-page

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,11 +3,13 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import {MatTableModule} from '@angular/material/table';
 const materialModules = [
   MatButtonModule,
   MatSliderModule,
   MatCardModule,
   MatIconModule,
+  MatTableModule
 ];
 
 @NgModule({
